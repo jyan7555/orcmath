5ca8fdb0877f2a3ab78dd9601d67e5ccf6fcd959
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-12-18 11:41:29 -0500

    GUI :)

--- OrcMathGUI/src/playground/CatalogMaker.java
+++ OrcMathGUI/src/playground/CatalogMaker.java
@@ -1,20 +1,23 @@
 package playground;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class CatalogMaker {
 	ArrayList<LeagueItems> list;
 	public CatalogMaker() {
 		list = new ArrayList<LeagueItems>();
-		list.add(new LeagueItems("Infinity Edge", 3400, "resources/infinityedge.png"));
+		//list.add(new LeagueItems("Infinity Edge", 3400, "resources/infinityedge.png"));
 		list.add(new LeagueItems("Statikk Shiv", 2600, "resources/statshiv.png"));
 	}
 
 	public static void main(String[] args) {
 		CatalogMaker catalog = new CatalogMaker();
+		catalog.addItem();
 		System.out.println(catalog.getCSVContent());
-
-	}
+		
+	}	
 	
 	public String getCSVContent() {
 		String data = "";
@@ -22,6 +25,19 @@ public class CatalogMaker {
 			data += item + "\n";
 		}
 		return data;
+	}
+	public void addItem() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What item do you want to add?");
+		String item = in.nextLine();
+		System.out.println("How much does it cost?");
+		int price = in.nextInt();
+		System.out.println("Source?");
+		String link = "";
+		while (link.equals("")) {
+			link = in.nextLine();
+		}
+		list.add(new LeagueItems(item, price, link));
 	}
 
 }
